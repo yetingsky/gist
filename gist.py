1cc78883f5c781a05c204795046510c3f6a10415
yetingsky <yeting19941229@gmail.com>
2017-09-22 18:55:47 +0800

    implement edit

--- gist.py
+++ gist.py
@@ -25,11 +25,11 @@ class GistClient(object):
     def get(self, id):
         pass
 
-    def create(self, file_names, description, public=True, anonymous=False):
+    def create(self, filenames, description, public=True, anonymous=False):
         files = {}
-        for file_name in file_names:
-            with open(file_name, 'r') as f:
-                files.setdefault(file_name, {})['content'] = f.read()
+        for filename in filenames:
+            with open(filename, 'r') as f:
+                files.setdefault(filename, {})['content'] = f.read()
 
         url = 'https://api.github.com/gists'
         data = {
@@ -48,8 +48,23 @@ class GistClient(object):
         gist_url = 'https://gist.github.com/{username}/{id}'
         return gist_url.format(username=self.username if not anonymous else 'anonymous', id=content['id'])
 
-    def edit(self):
-        pass
+    def edit(self, id, filenames, description):
+        files = {}
+        for filename in filenames:
+            with open(filename, 'r') as f:
+                files.setdefault(filename, {})['content'] = f.read()
+
+        url = 'https://api.github.com/gists/{id}'.format(id=id)
+        data = {
+            "description": description,
+            "files": files
+        }
+
+        r = requests.patch(url, json=data, auth=HTTPBasicAuth(self.username, self.auth_token))
+        content = json.loads(r.content)
+
+        gist_url = 'https://gist.github.com/{username}/{id}'
+        return gist_url.format(username=self.username, id=content['id'])
 
     def delete(self):
         pass
@@ -71,6 +86,15 @@ def cli():
 def create(files, description, public, anonymous):
     '''python gist.py create file1 file2 description --no-public --anonymous'''
     print client.create(files, description, public, anonymous)
+
+
+@cli.command()
+@click.argument('id', nargs=1)
+@click.argument('files', nargs=-1)
+@click.argument('description', nargs=1)
+def edit(id, files, description):
+    '''python gist.py edit id file1 file2 description'''
+    print client.edit(id, files, description)
 
 
 cli()
